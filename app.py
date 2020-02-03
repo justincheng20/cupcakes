0273fa134a09504493b4375ee25e5ebf706d6d18
Nicolas Guerrero <saynaysurf@gmail.com>
2020-02-03 15:26:50 -0800

    Added patch and delete routes in app.py and tests for these in tests.py

--- app.py
+++ app.py
@@ -8,43 +8,77 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 connect_db(app)
 db.create_all()
 
-def serialize_cupcake(cupcake):
-    return {
-        "id": cupcake.id,
-        "flavor": cupcake.flavor,
-        "size": cupcake.size,
-        "rating": cupcake.rating,
-        "image": cupcake.image
-    }
+
+
+
 
 @app.route("/api/cupcakes")
 def list_all_cupcakes():
+    """Return JSON {cupcakes: [{id, flavor, size, rating, image}, ...]} """
+    
     cupcakes = Cupcake.query.all()
-    serialized = [serialize_cupcake(c) for c in cupcakes]
+    serialized = [c.serialize() for c in cupcakes]
     return jsonify(cupcakes=serialized)
+
 
 @app.route("/api/cupcakes/<cupcake_id>")
 def list_single_cupcake(cupcake_id):
+    """Return JSON {cupcake: {id, flavor, size, rating, image}} """
+    
     cupcake = Cupcake.query.get_or_404(cupcake_id)
-    serialized = serialize_cupcake(cupcake)
+    serialized = cupcake.serialize()
     return jsonify(cupcake=serialized)
+
 
 @app.route("/api/cupcakes", methods=["POST"])
 def create_cupcake():
+    """Create cupcake from body data & return it. """
+    
     flavor = request.json["flavor"]
     size = request.json["size"]
     rating = request.json["rating"]
     image = request.json["image"]
 
-    new_cupcake = Cupcake(flavor=flavor, 
-                          size=size, 
-                          rating=rating, 
+    new_cupcake = Cupcake(flavor=flavor,
+                          size=size,
+                          rating=rating,
                           image=image)
     
     db.session.add(new_cupcake)
     db.session.commit()
 
-    serialized = serialize_cupcake(new_cupcake)
+    serialized = new_cupcake.serialize()
 
     return (jsonify(cupcake=serialized), 201)
 
+
+@app.route("/api/cupcakes/<cupcake_id>", methods=["PATCH"])
+def update_cupcake(cupcake_id):
+    """Update cupcake in database with updates from body of request.
+       Return JSON {cupcake: {id, flavor, size, rating, image}}. """
+    
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    cupcake.flavor = request.json["flavor"]
+    cupcake.size = request.json["size"]
+    cupcake.rating = request.json["rating"]
+    cupcake.image = request.json["image"]
+    
+    db.session.add(cupcake)
+    db.session.commit()
+
+    serialized = cupcake.serialize()
+
+    return(jsonify(cupcake=serialized), 200)
+
+
+@app.route("/api/cupcakes/<cupcake_id>", methods=["DELETE"])
+def delete_cupcake(cupcake_id):
+    """Delete cupdake. Return JSON {message: "Deleted"}."""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return (jsonify(message="Deleted"), 200)
--- tests.py
+++ tests.py
@@ -28,6 +28,13 @@ CUPCAKE_DATA_2 = {
     "image": "http://test.com/cupcake2.jpg"
 }
 
+UPDATE_CUPCAKE_DATA = {
+    "flavor": "UpdateFlavor",
+    "size": "UpdateSize",
+    "rating": 10,
+    "image": "http://test.com/cupcake2.jpg"
+}
+
 
 class CupcakeViewsTestCase(TestCase):
     """Tests for views of API."""
@@ -107,3 +114,41 @@ class CupcakeViewsTestCase(TestCase):
             })
 
             self.assertEqual(Cupcake.query.count(), 2)
+
+    def test_update_cupcake(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/{self.cupcake.id}"
+            initial_row_count = Cupcake.query.count()
+
+            resp = client.patch(url, json=UPDATE_CUPCAKE_DATA)
+
+            self.assertEqual(resp.status_code, 200)
+
+            data = resp.json
+            self.assertIsInstance(data['cupcake']['id'], int)
+            del data['cupcake']['id']
+
+            self.assertEqual(data, {
+                "cupcake": {
+                    "flavor": "UpdateFlavor",
+                    "size": "UpdateSize",
+                    "rating": 10,
+                    "image": "http://test.com/cupcake2.jpg"
+                }
+            })
+
+            self.assertEqual(Cupcake.query.count(), initial_row_count)
+
+    def test_delete_cupdake(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/{self.cupcake.id}"
+
+        resp = client.delete(url)
+
+        self.assertEqual(resp.status_code, 200)
+
+        data = resp.json
+        
+        self.assertEqual(data, {"message": "Deleted"})
+
+        self.assertEqual(Cupcake.query.count(), 0)
