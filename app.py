b556df372e8250a059e70fb289f9b1ee6b9b7389
justincheng20 <justincheng20@gmail.com>
2020-02-03 11:38:26 -0800

    Added cupcakes model and API routes for GET/POST

--- app.py
+++ app.py
@@ -1,1 +1,50 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, jsonify
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+
+connect_db(app)
+db.create_all()
+
+def serialize_cupcake(cupcake):
+    return {
+        "id": cupcake.id,
+        "flavor": cupcake.flavor,
+        "size": cupcake.size,
+        "rating": cupcake.rating,
+        "image": cupcake.image
+    }
+
+@app.route("/api/cupcakes")
+def list_all_cupcakes():
+    cupcakes = Cupcake.query.all()
+    serialized = [serialize_cupcake(c) for c in cupcakes]
+    return jsonify(cupcakes=serialized)
+
+@app.route("/api/cupcakes/<cupcake_id>")
+def list_single_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = serialize_cupcake(cupcake)
+    return jsonify(cupcake=serialized)
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    new_cupcake = Cupcake(flavor=flavor, 
+                          size=size, 
+                          rating=rating, 
+                          image=image)
+    
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = serialize_cupcake(new_cupcake)
+
+    return (jsonify(cupcake=serialized), 201)
+
--- models.py
+++ models.py
@@ -1,1 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+
 """Models for Cupcake app."""
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """"Connect to the database"""
+
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+
+    __tablename__ = "cupcakes"
+    
+    id = db.Column(db.Integer, 
+                   primary_key=True)
+    flavor = db.Column(db.Text, 
+                       nullable = False)
+    size = db.Column(db.Text, 
+                     nullable = False)
+    rating = db.Column(db.Float,
+                       nullable=False)
+    image = db.Column(db.Text,
+                      nullable=False,
+                      default="https://tinyurl.com/demo-cupcake")
